5895193a0e858eda9f0e31fa8f4acde156647a8d
saif__ee <36261367+saifbinabdulkareem@users.noreply.github.com>
2019-01-19 20:41:31 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCvv2YO-H7F9qXVWgFyreIibOV9Sx7fUIc",
+    authDomain: "realtymdbws.firebaseapp.com",
+    databaseURL: "https://realtymdbws.firebaseio.com",
+    projectId: "realtymdbws",
+    storageBucket: "realtymdbws.appspot.com",
+    messagingSenderId: "957313666949"
+  };
+  firebase.initializeApp(config);
+
